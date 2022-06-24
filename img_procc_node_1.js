7b8408e54b7a634aa5d504c3570c156138d565ea
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-25 02:12:09 +0700

    Update img_procc_node_1.js

--- img_procc_node_1.js
+++ img_procc_node_1.js
@@ -4,14 +4,7 @@ fetch('http://voalaelaina.ml/api/image.php', {
     method: 'POST',
     headers: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Cookie': 'staradmin2-free-banner=true; wschkid=d94953ac0bb92cc148cdb527e02b7d76881c7a56.1656130257.1',
-        'Origin': 'http://voalaelaina.ml',
-        'Referer': 'http://voalaelaina.ml/index.php',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44'
+        'Accept-Language': 'en-US,en;q=0.9'
     },
     body: new URLSearchParams({
         'type': 'https://upcdn.io/W142hRA6shGK3d38ivF9jT1',
