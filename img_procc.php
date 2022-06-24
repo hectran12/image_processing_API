ef2e93f3880780c8375f35c688aa6335305c8096
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-25 02:16:36 +0700

    Create img_procc.php

---
+++ img_procc.php
@@ -0,0 +1,16 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://voalaelaina.ml/api/image.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language' => 'en-US,en;q=0.9'
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=https%3A%2F%2Fupcdn.io%2FW142hRA6shGK3d38ivF9jT1&token=Dg0wTIRMi0VXpMgrqrXn0Nplm1QO25');
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
